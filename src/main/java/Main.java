2acc44509b07bd1a7ad14149fb35957fd9408c1f
KellyAyn <vojtech.aug@gmail.com>
2022-10-04 10:46:07 +0200

    Final changes for some time

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,11 +2,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Database.connect();
         Data.Init();
         System.out.println(Interface.help());
         Scanner sc = new Scanner(System.in);
-        while (true){
+        do {
             System.out.print("> ");
             switch (sc.next().trim().toLowerCase()) {
                 case "login" -> Interface.login();
@@ -16,6 +15,6 @@ public class Main {
                 case "help" -> System.out.println(Interface.help());
                 default -> System.out.println("Please input a valid command.");
             }
-        }
+        } while (true);
     }
 }
